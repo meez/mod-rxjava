47b91001570fb1a37c133e3e505fed3262cc2f01
Peter McDonnell <petermd@gmail.com>
2013-07-10 11:29:16 +0100

    add assertXX Observers

--- src/test/java/meez/rxvertx/java/RxTestSupport.java
+++ src/test/java/meez/rxvertx/java/RxTestSupport.java
@@ -1,12 +1,11 @@
 package meez.rxvertx.java;
 
-import org.vertx.java.testframework.TestUtils;
-import rx.util.functions.Action0;
-import rx.util.functions.Action1;
-import rx.util.functions.Func1;
-
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
+
+import org.vertx.java.testframework.TestUtils;
+import rx.Observer;
+import rx.util.functions.*;
 
 /** RxTestSupport */
 public class RxTestSupport {
@@ -130,6 +129,69 @@ public class RxTestSupport {
   public static Action0 testComplete(final TestUtils tu) {
     return new Action0() {
       public void call() {
+        tu.testComplete();
+      }
+    };
+  }
+  
+  /** Observer that expects a list */
+  public static <T> Observer<T> assertValues(final TestUtils tu, final T[] exp, final CountDownLatch latch) {
+    return new Observer<T>() {
+      private int idx=0;
+      public void onNext(T value) {
+        tu.azzert(idx<exp.length,"Too many results");
+        tu.azzert(exp[idx].equals(value),"Match result");
+        System.out.println("Got value: "+value);
+        idx++;
+      }
+      public void onCompleted() {
+        tu.azzert(idx==exp.length,"onCompleted() with values remaining");
+        latch.countDown();
+        // Call max once
+        idx++;
+      }
+      public void onError(Exception e) {
+        // Call never
+        tu.azzert(false,"Unexpected error (e="+e+")");
+        latch.countDown();
+      }
+    };
+  }
+
+  /** Observer that expects a single value */
+  public static <T> Observer<T> assertSingleValue(final TestUtils tu, final T exp, final CountDownLatch latch) {
+    return new Observer<T>() {
+      private int count=0;
+      public void onNext(T value) {
+        tu.azzert(count==0,"Single result");
+        tu.azzert(exp.equals(value),"Match result");
+        System.out.println("Got value: "+value);
+        count++;
+      }
+      public void onCompleted() {
+        tu.azzert(count==1,"onCompleted() with no value");
+        latch.countDown();
+      }
+      public void onError(Exception e) {
+        tu.azzert(false,"Unexpected error (e="+e+")");
+        latch.countDown();
+      }
+    };
+  }
+
+  /** Observer that expects an error of a given type */
+  public static <T> Observer<T> assertError(final TestUtils tu, final Class errType) {
+    return new Observer<T>() {
+      public void onNext(T value) {
+        System.out.println("Got value: "+value);
+      }
+      public void onCompleted() {
+        tu.azzert(false,"Error expected");
+      }
+      @SuppressWarnings("unchecked")
+      public void onError(Exception e) {
+        tu.azzert(errType.isAssignableFrom(e.getClass()));
+        System.out.println("Expected error: "+e);
         tu.testComplete();
       }
     };
